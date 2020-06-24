1c8b59538a82bfe9592bc0f2a128155000df1171
Maya Epps <mepps@fb.com>
2020-06-23 20:07:35 -0700

    Add final app submission

---
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -0,0 +1,79 @@
+package com.example.simpletodo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+// Responsible for displaying data from the model into a row in the recycler view
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+
+    public interface OnLongClickListener {
+        void onItemLongClicked(int position);
+    }
+
+    List<String> items;
+    OnLongClickListener longClickListener;
+
+    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener) {
+        this.items = items;
+        this.longClickListener = longClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        //use layout inflater to inflate a view
+        View todoView = LayoutInflater.from(viewGroup.getContext()).inflate(android.R.layout.simple_list_item_1, viewGroup, false);
+
+        //wrap it inside a View Holder and return it
+        return new ViewHolder(todoView);
+    }
+
+    //responsible for binding data to a particular view holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+
+        //grab the item at the position
+        String item = items.get(position);
+
+        //Bind the item into the specified view holder
+        viewHolder.bind(item);
+
+    }
+
+    //tells the RV how many items are on the list
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    //container to provide easy access to views that represent each row of the list
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvItem;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        //update the view inside of the view holder with this data
+        public void bind(String item) {
+            tvItem.setText(item);
+            tvItem.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    //Notify the listener which position was long pressed
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return false;
+                }
+            });
+        }
+    }
+}
